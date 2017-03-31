c1de61c472aad4d14d13c0a067d7d0f61588800e
newtonresearch <simon@newtonresearch.org>
2017-03-31 11:39:49 +0100

    Remove redundant code

--- NTX/PackagePart.h
+++ NTX/PackagePart.h
@@ -10,61 +10,7 @@
 #import "NewtonKit.h"
 #import "NTK/PackageParts.h"
 #import "NTK/ObjHeader.h"
-
-/*------------------------------------------------------------------------------
-	Legacy Refs for a 64-bit world.
-	See also Newton/ObjectSystem.cc which does this for Refs createwd by NTK.
-------------------------------------------------------------------------------*/
-
-typedef int32_t Ref32;
-
-#define OBJHEADER32 \
-	uint32_t size  : 24; \
-	uint32_t flags :  8; \
-	union { \
-		struct { \
-			uint32_t	locks :  8; \
-			uint32_t	slots : 24; \
-		} count; \
-		int32_t stuff; \
-		Ref32 destRef; \
-	}gc;
-
-struct ObjHeader32
-{
-	OBJHEADER32
-}__attribute__((packed));
-
-struct ArrayObject32
-{
-	OBJHEADER32
-	Ref32		objClass;
-	Ref32		slot[];
-}__attribute__((packed));
-
-struct SymbolObject32
-{
-	OBJHEADER32
-	Ref32		objClass;
-	ULong		hash;
-	char		name[];
-}__attribute__((packed));
-
-struct StringObject32
-{
-	OBJHEADER32
-	Ref32		objClass;
-	UniChar	str[];
-}__attribute__((packed));
-
-#define BYTE_SWAP_SIZE(n) (((n << 16) & 0x00FF0000) | (n & 0x0000FF00) | ((n >> 16) & 0x000000FF))
-#if defined(hasByteSwapping)
-#define CANONICAL_SIZE BYTE_SWAP_SIZE
-#else
-#define CANONICAL_SIZE(n) (n)
-#endif
-
-#define k4ByteAlignmentFlag 0x00000001
+#import "NTK/Ref32.h"
 
 /* -----------------------------------------------------------------------------
 	N T X P a c k a g e P a r t
@@ -87,7 +33,6 @@ struct StringObject32
 
 - (id)initWith:(RefArg)content type:(const char *)type alignment:(int)inAlignment;
 - (id)initWithRawData:(const void *)content size:(int)contentSize type:(const char *)type;
-- (void)updateInfoOffset:(ULong *)ioInfoOffset dataOffset:(ULong *)ioDataOffset;
 - (void)buildPartData:(NSUInteger)inBaseOffset;
 @end
 
