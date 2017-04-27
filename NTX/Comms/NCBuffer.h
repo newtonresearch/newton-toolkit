29a5e3b074c63e0998f400e2e04c8a498f9689f3
newtonresearch <simon@newtonresearch.org>
2017-04-27 14:40:55 +0100

    Code style changes

--- NTX/Comms/NCBuffer.h
+++ NTX/Comms/NCBuffer.h
@@ -12,39 +12,28 @@
 	N C B u f f e r
 ----------------------------------------------------------------------------- */
 
-#define kPageBufSize 1024
-
-
 @interface NCBufferAccess : NSObject
-{
-	unsigned char *	bufPtr;
-	unsigned int	index;
-	unsigned int	count;
-	unsigned int	lastCount;
-}
-@property (readonly) unsigned char * ptr;
-@property (readonly) unsigned int freeSpace;
-@property (readonly) unsigned int usedSpace;
-@property (assign) unsigned int count;
-@property (assign) int nextChar;
 
-- (void) setData: (unsigned char *) inData length: (NSUInteger) inLength;
-- (void) clear;
-- (void) reset;
-- (void) mark;
-- (void) refill;
+@property(readonly) unsigned char * ptr;
+@property(readonly) unsigned char * basePtr;
+@property(readonly) unsigned int freeSpace;
+@property(readonly) unsigned int usedSpace;
+@property(assign) unsigned int count;
+@property(assign) int nextChar;
 
-- (unsigned int) fill: (unsigned int) inAmount;
-- (unsigned int) fill: (unsigned int) inAmount from: (const void *) inBuf;
-- (void) drain: (unsigned int) inAmount;
-- (unsigned int) drain: (unsigned int) inAmount into: (void *) inBuf;
+- (void)setData:(unsigned char *)inData length:(NSUInteger)inLength;
+- (void)clear;
+- (void)reset;
+- (void)mark;
+- (void)refill;
+
+- (unsigned int)fill:(unsigned int)inAmount;
+- (unsigned int)fill:(unsigned int)inAmount from:(const void *)inBuf;
+- (void)drain:(unsigned int)inAmount;
+- (unsigned int)drain:(unsigned int)inAmount into:(void *)inBuf;
 
 @end
 
 
 @interface NCBuffer : NCBufferAccess
-{
-	unsigned char	buf[kPageBufSize];
-}
 @end
-
--- NTX/Comms/NCBuffer.m
+++ NTX/Comms/NCBuffer.m
@@ -14,135 +14,163 @@
 	index => limit of data read from buffer
 ----------------------------------------------------------------------------- */
 
+@interface NCBufferAccess ()
+{
+	unsigned int	index;
+	unsigned int	lastCount;
+}
+@property(assign) unsigned char * basePtr;
+@end
+
+#define kPageBufSize 1024
+
+@interface NCBuffer ()
+{
+	unsigned char buf[kPageBufSize];
+}
+@end
+
+
 @implementation NCBuffer
 
-- (id) init
-{
-	if (self = [super init])
-	{
-		bufPtr = buf;
+- (id)init {
+	if (self = [super init]) {
+		self.basePtr = buf;
 	}
 	return self;
 }
 @end
 
 
+/* -----------------------------------------------------------------------------
+	N C B u f f e r A c c e s s
+	Access to the buffer.
+----------------------------------------------------------------------------- */
+
 @implementation NCBufferAccess
 
-- (id) init
-{
-	if (self = [super init])
-	{
+- (id)init {
+	if (self = [super init]) {
 		[self clear];
 	}
 	return self;
 }
 
 
-- (void) setData: (unsigned char *) inData length: (NSUInteger) inLength
-{
-	bufPtr = inData;
-	count = inLength;
+- (void)setData:(unsigned char *)inData length:(NSUInteger)inLength {
+	self.basePtr = inData;
+	self.count = inLength;
 	[self reset];
 }
 
 
-- (void) clear
-{ [self reset]; count = 0; }
+- (void)clear {
+	[self reset];
+	self.count = 0;
+}
 
 
-- (void) reset
-{ index = 0; }
+- (void)reset {
+	index = 0;
+}
 
 
-- (void) mark
-{ lastCount = count; }
+- (void)mark {
+	lastCount = self.count;
+}
 
 
-- (void) refill
-{ [self reset]; count = lastCount; }
+- (void)refill {
+	[self reset];
+	self.count = lastCount;
+}
 
 
-- (unsigned char *) ptr
-{ return bufPtr + index; }
+- (unsigned char *)ptr {
+	return self.basePtr + index;
+}
 
 
-@synthesize count;
+- (unsigned int) freeSpace {
+	return kPageBufSize - self.count;
+}
 
 
-- (unsigned int) freeSpace
-{ return kPageBufSize - count; }
+- (unsigned int) usedSpace {
+	return self.count - index;
+}
 
 
-- (unsigned int) usedSpace
-{ return count - index; }
+- (int) nextChar {
+	if (index < self.count) {
+		return self.basePtr[index++];
+	} else {
+		[self clear];
+		return -1;
+	}
+}
 
 
-- (int) nextChar
-{ if (index < count) return bufPtr[index++]; else {[self clear]; return -1;} }
-
-
-- (void) setNextChar: (int) inCh
-{ if (count < kPageBufSize) bufPtr[count++] = inCh; }
+- (void)setNextChar:(int)inCh {
+	if (self.count < kPageBufSize) {
+		self.basePtr[self.count++] = inCh;
+	}
+}
 
 
 // return amount actually filled
-- (unsigned int) fill: (unsigned int) inAmount
-{
+- (unsigned int)fill:(unsigned int)inAmount {
 	unsigned int actualAmount = inAmount;
-	if (actualAmount > self.freeSpace)
+	if (actualAmount > self.freeSpace) {
 		actualAmount = self.freeSpace;
-	count += actualAmount;
+	}
+	self.count += actualAmount;
 	return actualAmount;
 }
 
 
-- (unsigned int) fill: (unsigned int) inAmount from: (const void *) inBuf
-{
+- (unsigned int)fill:(unsigned int)inAmount from:(const void *)inBuf {
 	unsigned int actualAmount = inAmount;
-	if (actualAmount > self.freeSpace)
+	if (actualAmount > self.freeSpace) {
 		actualAmount = self.freeSpace;
+	}
 	memcpy(self.ptr, inBuf, actualAmount);
 	[self fill:actualAmount];
 	return actualAmount;
 }
 
 
-- (void) drain: (unsigned int) inAmount
-{
+- (void)drain:(unsigned int)inAmount {
 	unsigned int actualAmount = inAmount;
-	if (actualAmount > self.usedSpace)
-	{
+	if (actualAmount > self.usedSpace) {
 		actualAmount = self.usedSpace;
 	}
 	index += actualAmount;
-	if (index == count)
+	if (index == self.count) {
 		[self clear];
+	}
 }
 
 
-- (unsigned int) drain: (unsigned int) inAmount into: (void *) inBuf
-{
+- (unsigned int)drain:(unsigned int)inAmount into:(void *)inBuf {
 	unsigned int actualAmount = inAmount;
-	if (actualAmount > self.usedSpace)
+	if (actualAmount > self.usedSpace) {
 		actualAmount = self.usedSpace;
+	}
 	memcpy(inBuf, self.ptr, actualAmount);
 	[self drain:actualAmount];
 	return actualAmount;
 }
 
 
-- (NSString *) description
-{
+- (NSString *)description {
 	char dbuf[1024];
-	int i, len = sprintf(dbuf, "NCBuffer index:%u, count:%u", index,count);
-	if (count > index)
-	{
+	int i, len = sprintf(dbuf, "NCBuffer index:%u, count:%u", index,self.count);
+	if (self.count > index) {
 		len += sprintf(dbuf+len, ", data:");
 		char * s = dbuf+len;
 		unsigned char * p = self.ptr;
-		for (i = index; i < count; i++, p++, s+=3)
-		{
+		for (i = index; i < self.count; i++, p++, s+=3) {
 			sprintf(s, " %02X", *p);
 		}
 		*s = 0;
